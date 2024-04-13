b3508679cfce196eb1dd76a133f1421c87baf139
BrendonJoaoAnalistadeSistema <brendonanalistadesistemas@gmail.com>
2024-04-12 21:22:02 -0300

    Primeiro código em Python: Hello World!!!

---
+++ aula001_primeiro_programa/hello_world.py
@@ -0,0 +1,19 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152 Elite do Senac
+# Autor: Brendon João Campos Neves
+# Data: 12/04/2024
+# Primeiro Programa em Python
+
+# Importando as bibliotecas
+import os
+
+
+# Limpando o terminal
+os.system( 'cls' )
+
+# O comando print() executa uma saída via terminal
+
+print( '_' * 70)
+print( 'Hello World!!!!' )
+print( '_' * 70)
+print()
