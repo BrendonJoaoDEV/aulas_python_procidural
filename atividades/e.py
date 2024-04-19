0dad44dfcea9d58d3d75840f61972819db59140a
BrendonJoaoAnalistadeSistemas <166769517+BrendonJoaoAnalistadeSistemas@users.noreply.github.com>
2024-04-19 08:27:38 -0300

    Atividade e.py

---
+++ atividades/e.py
@@ -0,0 +1,26 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma: 0152
+# Autor: Brendon João Campos Neves
+# Data: 19/04/2024
+# Faça um programa que receba um número inteiro e mostre o sucessor e antecessor.
+
+# Imports
+import os
+
+
+os.system('cls')
+
+# Entrada
+print('-'*70)
+numero = int(input('Digite um número inteiro: '))
+print('-'*70)
+
+# Processamento
+antecessor = numero - 1
+sucessor = numero + 1
+
+# Saída
+print('='*70)
+print(f'O antecessor de {numero} é: {antecessor}')
+print(f'O sucessor de {numero} é: {sucessor}')
+print('='*70)
