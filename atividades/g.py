f2a59d2d4844423fd4b395fdecf8db49a8163e7a
BrendonJoaoAnalistadeSistemas <166769517+BrendonJoaoAnalistadeSistemas@users.noreply.github.com>
2024-04-19 08:46:01 -0300

    Atividade g.py

---
+++ atividades/g.py
@@ -0,0 +1,27 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma: 0152
+# Autor: Brendon João Campos Neves
+# Data:19/04/2024
+# Faça um programa que converta metros em centímetros e milímetros.
+
+# Imports
+import os
+
+
+# Limpar o terminal
+os.system('cls')
+
+# Entrada
+print('-'*70)
+metros = float(input('Digite um valor em metros: '))
+print('-'*70)
+
+# Processamento
+centimetros = metros * 100
+milimetros = metros * 1000
+
+# Saída
+print('='*70)
+print(f'{metros} metros são {centimetros} centímetros.')
+print(f'{metros} metros são {milimetros} milímetros.')
+print('='*70)
