c5dc9481ac8b4a4ef8026a7daffce2c0a23c5db1
BrendonJoaoAnalistadeSistema <brendonanalistadesistemas@gmail.com>
2024-04-29 19:18:18 -0300

    Conclusão atividade

--- atividades/atividade002_condicionais/h.py
+++ atividades/atividade002_condicionais/h.py
@@ -7,7 +7,7 @@
 # Essas raízes são conhecidas como 𝑥’ = 5 e 𝑥’’ = 1, e o programa deve ser capaz de calcular essas raízes de forma precisa, seguindo os princípios matemáticos fundamentais.
 
 # Impotando as bibliotecas do sistema.
-import os 
+import os
 
 
 # Limpando o terminal.
@@ -21,4 +21,34 @@ print('.'*70)
 
 # Entrada de dados/Declaração de variáveis.
 print('-'*70)
-equacao
+a = 1
+b = -6
+c = 5
+delta = b**2 - 4*a*c
+bhaskara_mais = (-b + delta**0.5) / 2*a
+bhaskara_menos = (-b - delta**0.5) /2*a
+x_mais = ''
+x_menos = ''
+print('Dada a equação 𝑥²−6𝑥+5')
+print('-'*70)
+
+# Processamento.
+# Condicional:
+if delta > 0:
+    x_mais = f'A raiz + de 𝑥²−6𝑥+5 é {bhaskara_mais}'
+    x_menos = f'A raiz - de 𝑥²−6𝑥+5 é {bhaskara_menos}'
+else:
+    resposta = (f'Essa equação não pode ser realizada porque '
+                + f'delta não pode ser negativo ou zero')
+
+# Saída.
+print('='*70)
+print(x_mais)
+print(x_menos)
+print('='*70)
+
+# Título de fim.
+print('.'*70)
+print('Fim do programa!')
+print('.'*70)
+print()
