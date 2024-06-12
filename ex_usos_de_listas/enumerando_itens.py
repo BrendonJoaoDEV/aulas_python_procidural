2f7767515f4dc7449193b05b9b32de0a9294066a
BrendonJoaoAnalistadeSistemas <166769517+BrendonJoaoAnalistadeSistemas@users.noreply.github.com>
2024-06-11 21:35:56 -0300

    Create enumerando_itens.py

---
+++ ex_usos_de_listas/enumerando_itens.py
@@ -0,0 +1,21 @@
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('SAÍDA COM FOR... ENUMERATE()')
+print('=' * 70)
+
+soma = 0
+
+# Criando uma lista.
+lista_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Percorrendo a lista com o enumerate().
+# O comando enumerate adiciona um indice.
+# para cada valor de nossa lista.
+# Start opcional, para não começar no índice 0.
+# Enumerate(listaNumeros, start - 1)
+
+# Para cada número dentro da lista de números, enumere com um índice.
