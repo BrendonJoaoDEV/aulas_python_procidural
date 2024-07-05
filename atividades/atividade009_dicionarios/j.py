436ca6013dca24095502aa8a2d6f8d6af7acaee8
BrendonJoaoAnalistadeSistema <brendonanalistadesistemas@gmail.com>
2024-07-04 21:40:10 -0300

    atividade 009 continuação

--- atividades/atividade009_dicionarios/j.py
+++ atividades/atividade009_dicionarios/j.py
@@ -1,7 +1,7 @@
 # Curso de Desenvolvimento de Sistemas.
 # Turma: 0152.
 # Autor: Brendon João Campos Neves.
-# Data: 01/07/2024.
+# Data: 04/07/2024.
 # J) Faça um programa para criar um dicionário com nomes de frutas.
 # Em seguida verifique se tem abacaxi nos valores.
 
@@ -10,12 +10,12 @@ import os
 
 
 # Limpeza do terminal:
-os.system('clear')
+os.system('cls')
 
 # Declarações de variáveis:
+nao_abacaxi = True
 dicionario = {}
-chave = ''
-valor = ''
+chute = ''
 
 # Impressão do título:
 print('.'*79)
@@ -24,7 +24,29 @@ print('Tema: Frutas')
 print('.'*79)
 
 # Entrada de dados:
-while True:
-    print('(sistema) - Adivinhe a fruta que estou pensando...')
-    while True:
-        
+print('(sistema) - "Adivinhe a fruta que estou pensando...')
+while nao_abacaxi:
+    chute = str(input('Digite o nome de uma fruta: ')).strip().lower()
+    
+    # Processamento de dados:
+    dicionario[chute] = chute
+    if 'abacaxi' in dicionario.values():
+        nao_abacaxi = False
+        break
+    else:
+        print('Incorreto! Tente outra fruta')
+        continue
+
+
+# Saída de dados:
+print('='*79)
+print('Parabéns! Você acertou!')
+print('Frutas que você chutou: ')
+for valor in dicionario:
+    print(valor, end = ', ')
+print()
+print('='*79)
+
+print('.'*79)
+print('Fim do jogo! Obrigado por jogar!')
+print('.'*79)
---
+++ atividades/atividade009_dicionarios/k.py
@@ -0,0 +1,6 @@
+# Curso de Desenvolvimento de Sistemas.
+# Turma: 0152.
+# Autor: Brendon João Campos Neves.
+# Data: 04/07/2024.
+# K) Faça um programa que peça continuamente o nome e a idade de uma pessoa.
+# Caso o usuário digite a idade 999, o programa será finalizado e executará uma impressão com os nomes cadastrados.
