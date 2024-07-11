28d7685d24048dce04f23fbec1d00f7975c8baaa
BrendonJoaoAnalistadeSistemas <brendonanalistadesistemas@gmail.com>
2024-07-11 17:23:27 -0300

    atividade 010 - inicio

---
+++ atividades/atividade010_funcoes/a.py
@@ -0,0 +1,98 @@
+# Curso de Desenvolvimento de Sistemas.
+# Turma: 0152.
+# Data: 11/07/2024.
+# Autor: Brendon João Campoes Neves.
+# A) Crie uma função que receba uma lista de números.
+# Depois retorne duas listas com os números pares, os números ímpares,
+# a quantidade de números pares e a quantidade de números ímpares.
+
+# Definindo funções:
+def limpar_terminal():
+    import os
+    import platform
+
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+def pares_impares(lista_numeros):
+    pares = []
+    impares = []
+    for i in lista_numeros:
+        if i % 2 == 0:
+            pares.append(i)
+        else:
+            impares.append(i)
+
+        # Processo que remove elemento duplicados das listas:
+        pares = set(pares)
+        impares = set(impares)
+        pares = list(pares)
+        impares = list(impares)
+
+    return pares, len(pares), impares, len(impares)
+
+
+# Declarações de variáveis:
+numero = ''
+numeros = []
+opcao = ''
+
+# Limpando o terminal:
+limpar_terminal()
+
+print('.'*79)
+print('Separando números pares e ímpares:')
+print('.'*79)
+
+# Entrada de dados que pergunta ao usuário se deseja adicionar outro elemento:
+print('-'*79)
+while True:
+    numero = input('Digite os números que deseja verificar, um por um: ')
+    if numero.isnumeric():
+        numeros.append(int(numero))
+        opcao = str(input('Deseja adicionar outro número? (s/n) ')
+                    ).strip().lower()
+        if opcao == 's':
+            continue
+        else:
+            print('-'*79)
+            numeros = set(numeros)
+            numeros = list(numeros)
+            break
+    else:
+        print('Entrada inválida! Digite apenas números inteiros!')
+
+# Invocação da função com umpacking:
+pares, n_pares, impares, n_impares = pares_impares(numeros)
+
+# Ordenando listas:
+numeros.sort()
+pares.sort()
+impares.sort()
+
+# Saída dos resultados:
+print('='*79)
+print('Lista analisada:')
+for numero in numeros:
+    print(numero, end=', ')
+print()
+print('Números pares:')
+for n_par in pares:
+    print(n_par, end=', ')
+print()
+print('Números ímpares:')
+for n_impar in impares:
+    print(n_impar, end=', ')
+print()
+print('Na sua lista de números existem:')
+print(f'{n_pares} números pares.')
+print(f'{n_impares} números ímpares.')
+print('='*79)
+
+# Aviso de fim
+print('.'*79)
+print('Fim do programa! Obrigado ;)')
+print('.'*79)
---
+++ atividades/atividade010_funcoes/b.py
@@ -0,0 +1,71 @@
+# Curso de Desenvolvimento de Sistemas.
+# Turma: 0152.
+# Data: 11/07/2024.
+# Autor: Brendon João Campoes Neves.
+# B) Crie uma função que cadastre o nome de um aluno, a matrícula e
+# a data de nascimento. Depois imprima os resultados cadastrados
+# utilizando uma estrutura de repetição for.
+
+# Definições de funções:
+def limpar_terminal():
+    import os
+    import platform
+
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+def cadastrar_aluno(nome='', matricula=0, nascimento='00/00/0000'):
+    aluno = {}
+    aluno['nome'] = nome
+    aluno['matricula'] = matricula
+    aluno['nascimento'] = nascimento
+    
+    return aluno
+
+
+# Declarações de variáveis:
+nome = ''
+matricula = 0
+nascimento = ''
+aluno = {}
+escola = []
+opcao = ''
+
+print('.'*79)
+print('Cadastrar alunos: ')
+print('.'*79)
+
+# Entrada de dados:
+print('-'*79)
+while True:
+    nome = str(input('Digite o nome do aluno: ')).strip()
+    matricula = int(input('Digite o número de matrícula do aluno: '))
+    nascimento = str(input('Digite a data de nascimento do aluno (00/00/0000): '))
+    aluno.clear()
+    aluno = cadastrar_aluno(nome=nome, matricula=matricula, nascimento=nascimento)
+    escola.append(aluno.copy())
+    
+    opcao = str(input('Deseja adicionar outro aluno? (s/n)')).strip().lower()
+    print('~'*79)
+    if opcao == 's':
+        continue
+    else:
+        print('-'*79)
+        break
+
+# Saída de dados:
+print('='*79)
+print('Alunos da escola: ')
+for aluno in escola:
+    for chave, valor in aluno.items():
+        print(f'{chave}: {valor}')
+    print('~'*79)
+print('='*79)
+
+# Aviso de fim:
+print('.'*79)
+print('Fim do progrma! Obrigado ;)')
+print('.'*79)
