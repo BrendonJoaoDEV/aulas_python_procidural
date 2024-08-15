7e7033db4b80613279bf19b1b7f056da8083cd6e
BrendonJoaoDEVoficial <brendonjoaodev@gmail.com>
2024-08-14 21:39:50 -0300

    leitura e exclução de arquivos csv

---
+++ aula019_arquivos_csv/leitura.py
@@ -0,0 +1,37 @@
+# Curso de Desenvolvimento de Sistemas.
+# Turma: 0152.
+# Professor: Sebastião Marcos.
+# Data: 14/08/2024.
+
+import csv
+import os
+
+
+# Nome do arquivo CSV
+arquivo = 'aula019_arquivos_csv/gravacao/alunas.csv'
+
+# Lista para armazenar os dados
+lista = []
+
+# Abrindo o arquivo CSV para leitura
+with open(arquivo, 'r') as arquivo_csv:
+    # Criando um leitor de CSV que lê o arquivo como dicionário
+    leitura = csv.DictReader(arquivo_csv, delimiter=';')
+    
+    # Iterando sobre cada linha (registro) no arquivo CSV e adicionando à lista
+    for linha in leitura:
+        lista.append(linha)
+
+os.system('cls')
+print('-'*70)
+print('nome\ttelefone\tcidade')
+print('='*70)
+# Exibindo a lista resultante
+for registro in lista:
+    flag = 0
+    for k, v in registro.items():
+        print(v, end='\t')
+        flag += 1
+        if flag == 3:
+            print()
+print('-'*70)
